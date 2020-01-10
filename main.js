8394d75386838689b0d212b20220db5ad0eca181
bebyx <artem.bebyk@gmail.com>
2020-01-11 01:58:33 +0200

    Added time for New York

--- main.js
+++ main.js
@@ -9,6 +9,23 @@ function update() {
 	document.getElementById("h").innerHTML = hours;
 	document.getElementById("m").innerHTML = minutes;
 	document.getElementById("s").innerHTML = seconds;
+
+	//New York Time:
+	var dateNY = date.toLocaleString("en-US", {timeZone: "America/New_York"});
+    dateNY = new Date(dateNY);
+                
+    if (date == dateNY) console.log("hey, New Yorker!")
+                
+    let hoursNY = dateNY.getHours();
+	if (hoursNY < 10) hoursNY = "0" + hoursNY;
+	let minutesNY = dateNY.getMinutes();
+	if (minutesNY < 10) minutesNY = "0" + minutesNY;
+	let secondsNY = dateNY.getSeconds();
+	if (secondsNY < 10) secondsNY = "0" + secondsNY;
+		        
+	document.getElementById("h_ny").innerHTML = hoursNY;
+	document.getElementById("m_ny").innerHTML = minutesNY;
+	document.getElementById("s_ny").innerHTML = secondsNY;
 }
 
 setInterval(update, 1000);
